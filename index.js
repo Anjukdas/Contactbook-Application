68f2cdf43a52c11e7c8736c5efe680d4b1ca0127
AnjuKDas <anjukollara@gmail.com>
2025-08-02 11:55:37 +0530

    Updated UI with user cards and effects

--- index.js
+++ index.js
@@ -1,20 +1,49 @@
-const firstname = document.getElementById('firstname')
-const lastname = document.getElementById('secondname')
-const phone = document.getElementById('phonespan')
-const city = document.getElementById('cityspan')
-const email = document.getElementById('emailspan')
+const container = document.getElementById('user-container');
 
 async function getdata() {
-    const response = await fetch('https://fakestoreapi.com/users?authuser=2')
-    const data = await response.json();
-    console.log(data)
-    firstname.innerHTML = data[1].name.firstname;
-    lastname.innerHTML = data[1].name.lastname;
-    phone.innerHTML = data[1].name.phone;
-    city.innerHTML = data[1].name.cityspan;
-    email.innerHTML = data[1].name.emailspan;
+  const response = await fetch('https://fakestoreapi.com/users');
+  const data = await response.json();
 
+  data.forEach(user => {
+    const card = document.createElement('div');
+    card.className = 'card';
 
-    
+    // Profile image using pravatar
+    const img = document.createElement('img');
+    img.src = 'https://i.pravatar.cc/150?u=' + user.email;
+    img.alt = 'Profile Photo';
+
+    const fname = document.createElement('h2');
+    fname.textContent = user.name.firstname;
+
+    const lname = document.createElement('h3');
+    lname.textContent = user.name.lastname;
+
+    const phone = document.createElement('p');
+    phone.innerHTML = `<strong>Phone:</strong> ${user.phone}`;
+
+    const city = document.createElement('p');
+    city.innerHTML = `<strong>City:</strong> ${user.address.city}`;
+
+    const email = document.createElement('p');
+    email.innerHTML = `<strong>Email:</strong> ${user.email}`;
+
+    card.appendChild(img);
+    card.appendChild(fname);
+    card.appendChild(lname);
+    card.appendChild(phone);
+    card.appendChild(city);
+    card.appendChild(email);
+
+    // Click effect
+    card.addEventListener('click', () => {
+      card.classList.add('clicked');
+      setTimeout(() => card.classList.remove('clicked'), 400);
+    });
+
+    container.appendChild(card);
+  });
 }
+
 getdata();
+
